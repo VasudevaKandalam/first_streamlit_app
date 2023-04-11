c767777bddd01b4cce4b479c9572c16daa1bec60
VasudevaKandalam <tkvasu@gmail.com>
2023-04-10 19:36:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
+streamlit.stop()
 streamlit.title("My fist app /n")
 streamlit.header(" I want to become snowflake SME")
 streamlit.text("learning from Snowflake learn")
@@ -24,12 +26,17 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#jsonData=streamlit.text(fruityvice_response.json())
-jsonNormalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(jsonNormalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+  streamlit.write('The user entered ', fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  jsonNormalized=pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(jsonNormalized)
+except URLError as e:
+  streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
